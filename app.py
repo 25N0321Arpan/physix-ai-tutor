ed83088c577c718387833be1ca4950c16f10252c
25N0321Arpan <25N0321@iitb.ac.in>
2026-04-23 17:35:08 +0530

    Added admin dashboard

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ st.success("Welcome " + user)
 # ---------------------------
 # Admin Only
 # ---------------------------
-if user == "arpan":
+if user == "admin":
     st.markdown("---")
     if st.button("🔒 Open Admin Dashboard"):
         st.switch_page("admin_panel.py")
